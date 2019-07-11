8ef353fd0fd90830a1ef023d73f86cb0416017eb
Kuehar <green95251@gmail.com>
2019-07-11 23:36:55 +0900

    Review Tutorial and Rewrite Code from part1 to part4

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from . import  views
+from . import views
+
+# path()コールを追加して新しいViewをpolls.urlsモジュールと結びつける
+# URLConfに名前空間を追加する
+app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,43 @@
-from django.http import HttpResponse
+# HttpReponseRedirectの追加
+from django.http import HttpResponse,HttpReponseRedirect
+
+#データベースAPIの追加,Choiceの追加
+from .models import Question,Choice
+
+# renderをインポートし、templateを呼び出す内容を追加する
+from django.shortcuts import render
+
+#404エラーを送出できるようにする
+from django.http import Http404
+
+#ショートカット get_object_or_404を追加する
+from django.shortcuts import get_object_or_404
+
+from django.urls import reverse
+from django.views import generic
 
 def index(rewuest):
     return HttpResponse("Hello World!! You're at the polls index.")
+
+# 引数を取るViewを追加する
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    return HttpResponse("You're Voting on Question %s" % question_id)
